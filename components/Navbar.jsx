f5388c2bc52cc98ce04b57a100a4d7b8a7f928f3
Ugo BEN AMOR <ugobenamor@hotmail.com>
2022-09-16 16:07:43 +0200

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import {AiOutlineShopping} from 'react-icons/ai'
+import {Cart} from './';
+import { useStateContext } from '../context/StateContext';
+
+const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">Elo's Earrings</Link>
+      </p>
+      <button typeof='button' 
+      className='cart-icon' onClick={()=>setShowCart(true)}>
+          <AiOutlineShopping/>
+          <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+      {showCart && <Cart/>}
+    </div>
+  )
+}
+
+export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { Layout } from '../components'
+import {StateContext} from '../context/StateContext'
+import {Toaster} from 'react-hot-toast'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, children }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+   
+  
+  )
 }
 
 export default MyApp
